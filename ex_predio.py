002b577d766196aa06857dec4a9e7c54ce84ea0d
Bruna S Oliveira Giammelaro <138893476+brunagiammelaro@users.noreply.github.com>
2023-09-11 17:16:49 -0300

    Update and rename ex_predio.pyc to ex_predio.py

---
+++ ex_predio.py
@@ -0,0 +1,14 @@
+andar = 20
+while andar >= 1:
+  if andar != 13:
+    print (Andar)
+    andar -= 1
+
+for andar in range(20, 0, -1):
+    if andar != 13:
+        print(andar)
+
+numeros = [andar for andar in range(20, 0, -1) if andar != 13]
+for andar in numeros:
+    print(andar)
+
